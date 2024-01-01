9185eadf3a7949b94470e5594e58ad0cc818655f
avtandil <shainidze@gmail.com>
2024-01-01 15:18:27 +0100

    create function for call service

--- src/gemini/geminiService.js
+++ src/gemini/geminiService.js
@@ -41,8 +41,8 @@ async function vision(filePath) {
       'your response must be in JSON format and contain fields like {"number": meter_number, "value": meter_readings, "type": meter_type, "is_counter: boolean"}. There must not be any other characters other than the JSON string',
       "if the photo shows a counter then the field is_counter = true if the counter is not shown then is_counter = false",
       "you need to determine if the photo shows a counter.",
-      "you need to determine the counter indicators on the counter and write them in the value field.",
-      "you need to determine the meter number on the meter and write it in the number field.",
+      "you need to determine the counter indicators on the counter and write them in the value field. Ignore gray numbers",
+      "you need to determine the meter number on the meter and write it in the number field. Ignore gray numbers",
       "you need to determine the type of meter, for example: for water, for gas, for electricity. The value must be written in the type field.",
       ...imageParts,
     ]);
