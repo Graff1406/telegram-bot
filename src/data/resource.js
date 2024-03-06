4319a55a1485d74a6a1e7b1f3ea0831d49b77f24
avtandil <shainidze@gmail.com>
2024-03-06 14:18:36 +0100

    change ping server

--- src/data/resource.js
+++ src/data/resource.js
@@ -2,7 +2,7 @@ const links =
   "[facebook Channel](https://www.facebook.com/profile.php?id=61555984575841)\n[Telegram - Real State Channel](https://t.me/denona_real_estate)\n[Viber - Real State Channel](https://invite.viber.com/?g2=AQB5%2BBdfLYNcTVJh6%2FTEZgDJoDre8kePwPMUfs1j%2FjtAAUe4lr892SoFDv2BE32A)";
 
 const tbilisiLinks =
-  "[facebook Channel](https://www.facebook.com/profile.php?id=61555984575841)\n[Telegram - Real State Channel](https://t.me/tbilisi_propertiese)\n[Viber - Real State Channel](https://invite.viber.com/?g2=AQB5%2BBdfLYNcTVJh6%2FTEZgDJoDre8kePwPMUfs1j%2FjtAAUe4lr892SoFDv2BE32A)";
+  "[facebook Channel](https://www.facebook.com/profile.php?id=61555984575841)\n[Telegram - Real State Channel](https://t.me/tbilisi_properties)\n[Viber - Real State Channel](https://invite.viber.com/?g2=AQB5%2BBdfLYNcTVJh6%2FTEZgDJoDre8kePwPMUfs1j%2FjtAAUe4lr892SoFDv2BE32A)";
 
 const batumiLinks =
   "[facebook Channel](https://www.facebook.com/profile.php?id=61557183172067)\n[Telegram - Real State Channel](https://t.me/adjara_properties)\n[Viber - Real State Channel](https://invite.viber.com/?g2=AQAG16g7CzGETFKADynYyRGYKsc2qm52LaW9S2T64Pjp6MKBbe8wI2h%2FPtmfEmYH)";
