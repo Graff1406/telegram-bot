eb75837341acd06d31feb0e1444495151e257502
avtandil <shainidze@gmail.com>
2024-06-05 09:06:21 +0200

    add 365 pro

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,11 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./fb-key.json");
-console.log("🚀 ~ serviceAccount:", serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert({
     ...serviceAccount,
     private_key_id: process.env.PRIVATE_KEY_ID,
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    private_key: process.env.PRIVATE_KEY,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
 
